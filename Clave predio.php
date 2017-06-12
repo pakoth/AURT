468e6269984bd1df2ba530a4c4f84bdb0c750fd5
Casa-PC\Casa <pako_fthc@hotmail.com>
2017-06-11 23:56:23 -0500

    Version 1

---
+++ Clave predio.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+
+<body bgcolor="#3366FF">
+<center>
+<p><h1>Asociación de usuarios de riego de Tacámbaro</h1></p>
+<p><h2>Ingrese clave predio</h2></p>
+<p>
+    <input type="Clave predio" required value="Clave predio" onBlur="if(this.value=='')this.value='Clave predio'" onFocus="if(this.value=='Clave predio')this.value='' ">
+                </p>
+  <h2>
+    <input type="submit" value= " Buscar " class="button" />
+  </h2>
+</center>
+</body>
+</html>
---
+++ registroCliente.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Registro</title>
+</head>
+
+<body bgcolor="#33FF66">
+<center>
+<h1>Asociasion de usuarios de riego de Tacámbaro</h1>
+
+<h3>Registrarse de cliente</h3>
+<p>
+  <input type="nombre" required value="Nombre" onBlur="if(this.value=='')this.value='Nombre'" onFocus="if(this.value=='Nombre')this.value='' ">
+</p>
+<p>
+  <input type="apellido" required value="Apellido" onBlur="if(this.value=='')this.value='Apellido'" onFocus="if(this.value=='Apellido')this.value='' ">
+</p>
+<p>
+  <input type="Telefono" required value="Telefono" onBlur="if(this.value=='')this.value='Telefono'" onFocus="if(this.value=='Telefono')this.value='' ">
+</p>
+<p>
+  <input type="correo" required value="Correo" onBlur="if(this.value=='')this.value='Correo'" onFocus="if(this.value=='Correo')this.value='' ">
+</p>
+<p>
+  <input type="submit" value="Continuar" class="button" />
+</p>
+<p>&nbsp;</p>
+     </center>                   
+</body>
+</html>
